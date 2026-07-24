4894ce7ae302c28593361044de1867bab13c3bdf
KyroG4 <yeldeon@gmail.com>
2026-07-24 03:29:53 -0600

    crp mini juego
    crp mini juego

---
+++ Assets/Scripts/RCP.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using TMPro;
+using System;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+//NombreDeLaAccion.accion.triggered = true (para botones)
+//NombreDeLaAccion.accion.ReadValue<TipoDeDato>
+public class RCP : MonoBehaviour
+{
+    public float timer = 5f;
+    public float Max_time = 30f;
+    public float beat_duration = 0.5f;
+    public float epsilon = 0.1f;
+    public TMP_Text texto;
+    public Image heart;
+    private bool good;
+    public int Max_good = 10;
+    public int good_count = 0;
+    public Image feel_bar;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        texto.text = Convert.ToString(Convert.ToInt32(timer));
+        float passed = Max_time - timer;
+        float beat = passed % beat_duration;
+        if (beat >= beat_duration - epsilon && beat <= beat_duration)
+        {
+            heart.color = Color.red;
+            good = true;
+        }
+        else if (beat >= 0 && beat <= epsilon)
+        {
+            heart.color = Color.red;
+        }
+        else
+        {
+            heart.color = Color.black;
+            good = false;
+        }
+
+
+
+        if (timer <= 0)
+            {
+                Debug.Log("y tu tiempo se acabo");
+                timer = 30f;
+            }
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (good == true)
+        {
+            Debug.Log("good");
+            good_count++;
+            feel_bar.fillAmount = (float)good_count / (float)Max_good;
+        }
+        else
+        {
+            Debug.Log("hmm estas mal");
+        }
+    }
+}
